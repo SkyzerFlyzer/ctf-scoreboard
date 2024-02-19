b2a32562a9ba3b313798dda5e0fe9d4985b26226
SkyzerFlyzer <skyzerflyzer@gmail.com>
2024-02-19 11:31:45 +0000

    Changed scoreboard urls to root instead of /ctf

--- CTFScoreboard/urls.py
+++ CTFScoreboard/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ctf/', include('scoreboard.urls'))
+    path('', include('scoreboard.urls'))
 ]
