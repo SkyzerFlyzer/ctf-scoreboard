5ff214239160f1ad065dbb450d0050ea4f8eac4b
SkyzerFlyzer <skyzerflyzer@gmail.com>
2024-02-20 11:40:01 +0000

    Added a verbose name

--- scoreboard/apps.py
+++ scoreboard/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 class ScoreboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scoreboard'
+    verbose_name = "django-ctf-scoreboard"
 
 
